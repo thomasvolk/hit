827426d0390c72a95c229ab2292961d7424b089e
Thomas Volk (MacBook) <info@thomasvolk.de>
2025-05-03 22:07:09 +0200

    oi test

--- test/test_io.ml
+++ test/test_io.ml
@@ -1,10 +1,29 @@
 open OUnit2
+open Hit
+
+module TermIndexIo = Io.Make(Io.TermIndexFile)
+
+let add_entry r l ti =
+  let open Hit.Index in
+  TermIndex.add (TermIndex.Entry.create (Ref.create r) l) ti 
+
+let test_path = "./test_io/index"
 
 let tests =
   "Io" >::: [
     "load" >:: (
       fun _ ->
-        assert_equal ~printer:Fun.id ""  ""
+        let cfg = Io.TermIndexFile.create test_path in
+        let ti = TermIndexIo.load "test" cfg 
+        |> add_entry "notes::main.md" [1; 2; 3]
+        |> add_entry "notes::x.md" [8; 23; 89]
+        |> add_entry "notes::a/b/foo.md" [34; 200; 387] in
+        TermIndexIo.save ti cfg;
+        let expected = {|3f61a33051c00c43956ca8b798ca651e 8 23 89
+58bc212a2d19e9b88ec655e5d2194dd7 34 200 387
+e4fb6111620be10611cf5a25e38339d4 1 2 3
+|} in
+        assert_equal ~printer:Fun.id expected (Io.read_file (Filename.concat test_path "term-index/09/8f/6b/cd/4621d373cade4e832627b4f6"));
     );
   ]
 
