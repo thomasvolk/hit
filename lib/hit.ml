ef83a6c2ddeb1946468697fb5ffde97fd007dc8c
Thomas Volk (MacBook) <info@thomasvolk.de>
2025-05-03 22:08:57 +0200

    cleanup

--- lib/hit.ml
+++ lib/hit.ml
@@ -1,7 +1,4 @@
 module Token = Token
 module Index = Index
-module Term = Term
-module Util = Util
-module Doc= Doc
 module Io = Io
 module Ref = Ref
--- lib/util.mli
+++
@@ -1,8 +0,0 @@
-
-module Hash : sig
-  type t = string
-
-  val create : string -> t
-
-  val to_path : t -> string
-end
