e164d3a47431728a6ca206630784e4efb7a4ff22
Thomas Volk (MacBook) <info@thomasvolk.de>
2025-06-05 23:44:24 +0200

    error

--- test/test_index.ml
+++ test/test_index.ml
@@ -15,6 +15,7 @@ let tests =
         [
           create (Meta.create "local" "docs/test01.txt") "my test document 01 x";
           create (Meta.create "local" "docs/test02.txt") "my test test document 02 01 foo bar x 2 x";
+          create (Meta.create "local" "docs/test03.txt") "test 3";
         ] in
       let _i' = docs |> List.fold_left (fun i d -> Idx.add_doc d i) i in
       ()
