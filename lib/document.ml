bf34fa4ded83ecfb60b1baa1ab36eff1cb4dfc7f
Thomas Volk (MacBook) <info@thomasvolk.de>
2025-11-04 18:34:43 +0100

    separate document module

---
+++ lib/document.ml
@@ -0,0 +1,41 @@
+open Sexplib.Std
+
+module Checksum = struct
+  type t = string [@@deriving sexp]
+
+  let create cnt = Digest.MD5.string cnt |> Digest.MD5.to_hex
+end
+
+module DocumentId = struct
+  let prefix = "doc"
+end
+
+module Id = Reference.Make (DocumentId)
+
+module Meta = struct
+  type t = { source : string; path : string; checksum : Checksum.t }
+  [@@deriving sexp]
+
+  let make_reference s p = s ^ "::" ^ p
+  let create s p c = { source = s; path = p; checksum = c }
+  let path m = m.path
+  let source m = m.source
+  let reference m = make_reference m.source m.path
+  let id m = Id.create (reference m)
+  let checksum m = m.checksum
+end
+
+type t = { meta : Meta.t; content : string }
+
+let create m c = { meta = m; content = c }
+
+let from_source s p c =
+  let m = Meta.create s p (Checksum.create c) in
+  { meta = m; content = c }
+
+let content d = d.content
+let meta d = d.meta
+let id d = Meta.id d.meta
+let checksum d = d.meta.checksum
+
+module DocumentMap = Map.Make (Id)
