a291684e1929d611ad7ef021db57592cdab7fa8a
Thomas Volk (MacBook) <info@thomasvolk.de>
2026-01-10 00:49:47 +0100

    init file index function for benchmark

--- bin/hit_benchmark.ml
+++ bin/hit_benchmark.ml
@@ -6,25 +6,26 @@ let benchmark index_path =
     print_endline ("ERROR: Directory already exists: " ^ index_path);
     exit 1);
   Sys.mkdir index_path 0o755;
-  let module S = (val Io.file_storage index_path : Io.StorageInstance) in
-  let module Idx = Index.Make (S) in
-  let module Q = Index.Query.Make (Idx) in
-  ignore (Idx.init ());
-  let init =
-    let idx = Idx.load () in
-    let idx = Idx.clear idx in
+  let module FileStore = (val Io.file_storage index_path : Io.StorageInstance)
+  in
+  let module FileIdx = Index.Make (FileStore) in
+  let module FileIdxQuery = Index.Query.Make (FileIdx) in
+  ignore (FileIdx.init ());
+  let init_file_index =
+    let idx = FileIdx.load () in
+    let idx = FileIdx.clear idx in
     let idx =
       idx
-      |> Idx.add_doc
+      |> FileIdx.add_doc
            (Document.from_source "local" "/documents/doc1.txt"
               "This is the content of document one.")
-      |> Idx.add_doc
+      |> FileIdx.add_doc
            (Document.from_source "local" "/documents/doc2.txt"
               "This is the content of document two.")
-      |> Idx.add_doc
+      |> FileIdx.add_doc
            (Document.from_source "local" "/documents/doc3.txt"
               "This is the content of document three.")
-      |> Idx.add_doc
+      |> FileIdx.add_doc
            (Document.from_source "local" "/documents/doc4.txt"
               "This is the very long content\n\
               \                                                       Lorem \
@@ -36,20 +37,24 @@ let benchmark index_path =
               \                                                       of \
                document four.")
     in
-    Idx.flush idx
+    FileIdx.flush idx
+  in
+  let with_file_index f =
+   fun `init ->
+    let idx = init_file_index in
+    fun () -> f idx
   in
   [
-    Bench.Test.create_with_initialization ~name:"Query.find_docs" (fun `init ->
-        let idx = init in
-        fun () -> Q.find_docs [ "document"; "one" ] idx);
-    Bench.Test.create_with_initialization ~name:"Index.add_doc (existing)"
-      (fun `init ->
-        let idx = init in
-        fun () ->
-          Idx.add_doc
-            (Document.from_source "local" "/documents/doc1.txt"
-               "This is the content of document one.")
-            idx);
+    (fun idx -> FileIdxQuery.find_docs [ "document"; "one" ] idx)
+    |> with_file_index
+    |> Bench.Test.create_with_initialization ~name:"Query.find_docs";
+    (fun idx ->
+      FileIdx.add_doc
+        (Document.from_source "local" "/documents/doc1.txt"
+           "This is the content of document one.")
+        idx)
+    |> with_file_index
+    |> Bench.Test.create_with_initialization ~name:"Index.add_doc (existing)";
   ]
 
 let () =
