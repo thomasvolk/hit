61b3741f86c2890adc985fc1a6e7c5321283419a
Thomas Volk (MacBook) <info@thomasvolk.de>
2026-01-16 23:18:40 +0100

    add Memory benchmarks

--- bin/hit_benchmark.ml
+++ bin/hit_benchmark.ml
@@ -1,7 +1,7 @@
 open Core_bench
 open Hit
 
-let benchmark (module Idx : Index.IndexType)
+let benchmark name (module Idx : Index.IndexType)
     (module Query : Index.Query.QueryType) =
   let init_index =
     ignore (Idx.create ());
@@ -39,14 +39,14 @@ let benchmark (module Idx : Index.IndexType)
   in
   [
     with_index (fun idx -> Query.find_docs [ "document"; "one" ] idx)
-    |> Bench.Test.create_with_initialization ~name:"Filesystem: Query.find_docs";
+    |> Bench.Test.create_with_initialization ~name: (name ^ ": Query.find_docs");
     with_index (fun idx ->
         Idx.add_doc
           (Document.from_source "local" "/documents/doc1.txt"
              "This is the content of document one.")
           idx)
     |> Bench.Test.create_with_initialization
-         ~name:"Filesystem: Index.add_doc (existing)";
+         ~name: (name ^ ": Index.add_doc (existing)");
   ]
 
 let () =
@@ -61,5 +61,12 @@ let () =
   in
   let module FileIdx = Index.Make (FileStore) in
   let module FileIdxQuery = Index.Query.Make (FileIdx) in
+  let module MemoryStore =
+    (val Io.in_memory_storage (100, 100) : Io.StorageInstance)
+  in
+  let module MemoryIdx = Index.Make (MemoryStore) in
+  let module MemoryIdxQuery = Index.Query.Make (MemoryIdx) in
   Command_unix.run
-    (Bench.make_command (benchmark (module FileIdx) (module FileIdxQuery)))
+    (Bench.make_command
+       (benchmark "Filesystem" (module FileIdx) (module FileIdxQuery)
+       @ benchmark "Memory" (module MemoryIdx) (module MemoryIdxQuery)))
