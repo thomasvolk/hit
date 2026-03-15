ebef5106717e8dd78df03489cda5c5b1852bc06d
Thomas Volk (MBP-von-Thomas.fritz.box) <info@thomasvolk.de>
2026-03-15 12:11:22 +0100

    Simplify split to tokens

--- lib/token.ml
+++ lib/token.ml
@@ -1,22 +1,17 @@
 let token_chars_start = 0x30
 
-let split s =
-  let r = ref [] in
-  let j = ref (String.length s) in
-  for i = String.length s - 1 downto 0 do
-    let current = String.unsafe_get s i in
-    if current < Char.chr token_chars_start then (
-      r := String.sub s (i + 1) (!j - i - 1) :: !r;
-      j := i)
-  done;
-  String.sub s 0 !j :: !r
 
-let get_tokens ?(min_token_length = 2) s =
-  let is_not_empty (w, _) = String.length w >= min_token_length in
-  split s
-  |> List.fold_left
-       (fun (l, c) t -> (List.append [ (t, c) ] l, c + 1 + String.length t))
-       ([], 0)
-  |> fst |> List.rev |> List.filter is_not_empty
-  |> List.map (fun (w, c) -> (String.lowercase_ascii w, c))
+let from_string s =
+  let split s =
+    let r = ref [] in
+    let j = ref (String.length s) in
+    for i = String.length s - 1 downto 0 do
+      let current = String.unsafe_get s i in
+      if current < Char.chr token_chars_start then (
+        r := String.sub s (i + 1) (!j - i - 1) :: !r;
+        j := i)
+    done;
+    String.sub s 0 !j :: !r
+  in
+  split s |> List.filter (fun s -> String.length s > 0)
 
