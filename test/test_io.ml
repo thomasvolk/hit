4c0bc66a07c79251801f189cadfb559adacd4c4c
Thomas Volk (MacBook) <info@thomasvolk.de>
2026-03-22 19:57:53 +0100

    comments

--- test/test_io.ml
+++ test/test_io.ml
@@ -19,11 +19,15 @@ let tests = [
     |> Io.Trx.add (Io.Action.of_delete_file (Filename.concat test_dir "testfile03.txt"))
     in
     let trx_path = (Filename.concat test_dir "trx.hit") in
+    (* write the transaction to the file system *)
     Io.write_file_from_sexp trx_path (Io.Trx.sexp_of_t trx);
+    (* try to execute the transaction *)
     assert_raises (Io.TransactionError "transaction already exists: ./test_io_trx/trx.hit") (fun () ->
        Io.execute_transaction trx_path trx
     );
+    (* read the transaction from file *)
     let trx' = Io.Trx.t_of_sexp (Io.read_file_to_sexp trx_path) in
+    (* execute the transaction without check *)
     Io.execute_transaction ~check_for_existing:false trx_path trx'
   ));
 ]
