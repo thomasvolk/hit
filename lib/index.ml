9bcf52cb03e2df2c749a36634ce0dac4a8a047ad
Thomas Volk (MBP-von-Thomas.fritz.box) <info@thomasvolk.de>
2026-03-27 18:17:36 +0100

    add delete doc

--- lib/index.ml
+++ lib/index.ml
@@ -1,4 +1,5 @@
 open Sexplib.Std
+open Io
 
 module Query = struct
   type t =
@@ -17,16 +18,30 @@ module Query = struct
 end
 
 type t = { path : string }
+type doc_files = { tokens_file : string; doc_file : string }
+
+let fold_left f l t = List.fold_left f t l
 
 let create path = { path }
 
-let add_doc t doc words =
-  let fold_left f l t = List.fold_left f t l in
+let doc_files t doc_id = 
+  let doc_dir = t.path // Doc.Id.to_path doc_id in
+  { tokens_file = doc_dir // "tokens.hit" ; doc_file = doc_dir // "doc.hit" }
+
+let token_entry_file t doc_id token_id = 
+  (t.path // Token.Id.to_path token_id // Doc.Id.to_string doc_id) ^ ".hit"
+
+let execute t doc_id trx =
+  execute_transaction
+    (t.path // "trx" // (Doc.Id.to_string doc_id ^ ".hit"))
+    trx
+
+let add t ?(tokenizer=Token.from_string) path content =
+  let words = tokenizer path @ tokenizer content
+  and doc = Doc.create path (Doc.Checksum.create content) in
   let module TokenMap = Map.Make(Token.Id) in 
-  let open Io in
   let doc_id = Doc.Id.create (Doc.path doc) in
-  let token_doc_entry_path token_id =
-    (t.path // Token.Id.to_path token_id // Doc.Id.to_string doc_id) ^ ".hit"
+  let get_token_doc_entry_file = token_entry_file t doc_id
   in
   let tokens =
     Token.with_orders words
@@ -34,11 +49,10 @@ let add_doc t doc words =
         ( Token.Id.create (fst e),
           (Token.create (fst e), Token.DocumentEntry.create (snd e)) ))
   in
-  let doc_dir = Doc.Id.to_path doc_id in
-  let doc_tokens_file = t.path // doc_dir // "tokens.hit" in
+  let df = doc_files t doc_id in
   let current_doc_tokens =
-    if file_exists doc_tokens_file then
-      read_file_to_sexp doc_tokens_file |> Doc.TokenRefs.t_of_sexp
+    if file_exists df.tokens_file then
+      read_file_to_sexp df.tokens_file |> Doc.TokenRefs.t_of_sexp
     else Doc.TokenRefs.empty
   in
   let token_map = TokenMap.of_list tokens in
@@ -47,30 +61,34 @@ let add_doc t doc words =
     List.map fst tokens
     |> List.fold_left (fun acc e -> Doc.TokenRefs.add e acc) Doc.TokenRefs.empty
   in
-  let trx =
-    Trx.empty
+  Trx.empty
     |> fold_left
-         (fun acc e -> Trx.add_delete_file (token_doc_entry_path e) acc)
+         (fun acc e -> Trx.add_delete_file (get_token_doc_entry_file e) acc)
          tokens_to_delete
-    |> Trx.add_write_file doc_tokens_file
+    |> Trx.add_write_file df.tokens_file
          (Doc.TokenRefs.sexp_of_t new_doc_tokens)
-    |> Trx.add_write_file (t.path // doc_dir // "doc.hit") (Doc.sexp_of_t doc)
+    |> Trx.add_write_file df.doc_file (Doc.sexp_of_t doc)
     |> fold_left
          (fun acc e ->
            Trx.add_write_file
-             (token_doc_entry_path (fst e))
+             (get_token_doc_entry_file (fst e))
              (Token.DocumentEntry.sexp_of_t (snd (snd e)))
              acc
            |> Trx.add_write_file
                 (t.path // Token.Id.to_path (fst e) // "token.hit")
                 (fst (snd e) |> Token.sexp_of_t))
          tokens
-  in
-  execute_transaction
-    (t.path // "trx" // (Doc.Id.to_string doc_id ^ ".hit"))
-    trx
+    |> execute t doc_id
 
-let add t path content =
-  let tokens = Token.from_string path @ Token.from_string content
-  and doc = Doc.create path (Doc.Checksum.create content) in
-  add_doc t doc tokens
+let delete t doc_id =
+  let df = doc_files t doc_id in
+  let doc_tokens = Io.read_file_to_sexp df.tokens_file |> Doc.TokenRefs.t_of_sexp in
+  let get_token_doc_entry_file = token_entry_file t doc_id in
+  Trx.empty
+    |> fold_left
+         (fun acc e -> Trx.add_delete_file (get_token_doc_entry_file e) acc)
+         doc_tokens
+    |> Trx.add_delete_file df.doc_file
+    |> Trx.add_delete_file df.tokens_file
+    |> execute t doc_id
+
