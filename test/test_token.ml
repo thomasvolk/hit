ef1f76c1b93a91204b38a9485c90ad5e43aca13e
Thomas Volk (trutvo-vm) <info@thomasvolk.de>
2026-03-31 17:03:36 +0200

    more token tests

--- test/test_token.ml
+++ test/test_token.ml
@@ -15,14 +15,16 @@ let tests =
       assert_equal 1 (List.length (with_orders "one one"));
       assert_equal 2 (List.length (with_orders "one one two"));
       let s =
-        "/0/1/22/3/44.txt 1 \t44 \r foo  bar äöü,ßß.foo test test \n\
+        "/0/1/22/3/44.txt 1 \t44 \r FOO  Bar äöü,ßß.foo test test \n\
         \ 测试 foo äöü test end"
       in
       assert_bool "string is not valid utf8" (String.is_valid_utf_8 s);
       let tl = with_orders s in
       assert_equal ~printer:string_of_int 10 (List.length tl);
       assert_equal ("22", [ 0 ]) (List.nth tl 0);
-      assert_equal ("44", [ 1; 3 ]) (List.nth tl 1) );
+      assert_equal ("44", [ 1; 3 ]) (List.nth tl 1);
+      assert_equal ("bar", [ 5 ]) (List.nth tl 2);
+      assert_equal ("foo", [ 4; 8; 12 ]) (List.nth tl 4) );
   ]
 
 let _ = run_test_tt_main ("Token" >::: tests)
