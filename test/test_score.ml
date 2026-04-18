3d075dd3f3a4195847cd9e1137da7022885c2fb2
Thomas Volk (MacBook) <info@thomasvolk.de>
2026-04-18 10:55:55 +0200

    return option

--- test/test_score.ml
+++ test/test_score.ml
@@ -1,25 +1,27 @@
 open OUnit2
 open Hit.Score
 
-let int_list_printer lst =
-  let elements = String.concat "; " (List.map string_of_int lst) in
-  Printf.sprintf "[%s]" elements
+let int_list_printer = function
+  | Some (lst, span) ->
+      let elements = String.concat "; " (List.map string_of_int lst) in
+      Printf.sprintf "[%s] span=%n" elements span
+  | None -> "None"
 
 let tests =
   [
     ( "find_closest_elements" >:: fun _ ->
       let rows = [ [ 1; 2; 3 ]; [ 4; 5; 6 ]; [ 7; 8; 9 ] ] in
-      let expected = [ 3; 4; 7 ] in
+      let expected = Some ([ 3; 4; 7 ], 4) in
       let result = find_closest_elements rows in
       assert_equal ~printer:int_list_printer expected result );
     ( "find_closest_elements with empty input" >:: fun _ ->
       let rows = [] in
-      let expected = [] in
+      let expected = None in
       let result = find_closest_elements rows in
       assert_equal ~printer:int_list_printer expected result );
     ( "find_closest_elements with single row" >:: fun _ ->
       let rows = [ [ 10; 20; 30 ] ] in
-      let expected = [ 10 ] in
+      let expected = Some ([ 10 ], 0) in
       let result = find_closest_elements rows in
       assert_equal ~printer:int_list_printer expected result );
   ]
