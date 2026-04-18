e2cfc8098c11d9e1df805568559ee93188c2a8c3
Thomas Volk (MacBook) <info@thomasvolk.de>
2026-04-18 10:57:47 +0200

    rename method

--- test/test_score.ml
+++ test/test_score.ml
@@ -1,7 +1,7 @@
 open OUnit2
 open Hit.Score
 
-let int_list_printer = function
+let closest_elements_printer = function
   | Some (lst, span) ->
       let elements = String.concat "; " (List.map string_of_int lst) in
       Printf.sprintf "[%s] span=%n" elements span
@@ -13,17 +13,17 @@ let tests =
       let rows = [ [ 1; 2; 3 ]; [ 4; 5; 6 ]; [ 7; 8; 9 ] ] in
       let expected = Some ([ 3; 4; 7 ], 4) in
       let result = find_closest_elements rows in
-      assert_equal ~printer:int_list_printer expected result );
+      assert_equal ~printer:closest_elements_printer expected result );
     ( "find_closest_elements with empty input" >:: fun _ ->
       let rows = [] in
       let expected = None in
       let result = find_closest_elements rows in
-      assert_equal ~printer:int_list_printer expected result );
+      assert_equal ~printer:closest_elements_printer expected result );
     ( "find_closest_elements with single row" >:: fun _ ->
       let rows = [ [ 10; 20; 30 ] ] in
       let expected = Some ([ 10 ], 0) in
       let result = find_closest_elements rows in
-      assert_equal ~printer:int_list_printer expected result );
+      assert_equal ~printer:closest_elements_printer expected result );
   ]
 
 let _ = run_test_tt_main ("Score" >::: tests)
